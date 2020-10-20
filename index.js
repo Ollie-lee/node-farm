ae27acab87a33e57108214af752f332e4deb029d
Ollie <myself.ollie.lee@gmail.com>
2020-10-20 18:20:46 +1100

    simple server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 //blocking sync
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn);
@@ -10,20 +11,36 @@ const fs = require('fs');
 // console.log('file has been written');
 
 //non-blocking, async
-fs.readFile('./txt/stdart.txt', 'utf-8', (err, data1) => {
-  if (err) {
-    return console.log('ERROR');
-  }
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/stdart.txt', 'utf-8', (err, data1) => {
+//   if (err) {
+//     return console.log('ERROR');
+//   }
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      //write all of data together
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('final file has been written');
-      })
-    })
-  })
+//       //write all of data together
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('final file has been written');
+//       })
+//     })
+//   })
+// })
+// console.log('file has been written');
+
+// --------------------------SERVER------------
+//step1: create a server
+const server = http.createServer((req, res) => {
+  //each time request hit server, this call back is called
+  console.log(req);
+  res.end('Hello from server')//send back a very simple response
 })
-console.log('file has been written');
+
+//step2: listen to incoming request from client on localhost IP's specific port
+//port: a subAddress of host
+//default localhost IP address:127.0.0.1(local host is current computer)
+server.listen(8000, '127.0.0.1', () => {
+  //call run when start listening
+  console.log('Listening to request on 8000');
+})
