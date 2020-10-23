d73d563670c52f5e99457b8659f974de6d4451fc
Ollie <myself.ollie.lee@gmail.com>
2020-10-23 20:28:24 +1100

    add prettier config

--- index.js
+++ index.js
@@ -41,20 +41,28 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 //top level code only get executed once
 //so we use sync version, cuz it's easier to handle data
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
-const dataObj = JSON.parse(data)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
-
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 //here the callback is executed when there is a new request
 const server = http.createServer((req, res) => {
   //each time request hit server, this call back is called
-  const pathName = req.url
-  const { pathname, query } = url.parse(pathName, true);//second: parse query string to obj
+  const pathName = req.url;
+  const { pathname, query } = url.parse(pathName, true); //second: parse query string to obj
   // console.log(pathName);
   // console.log(pathname);
   // console.log(query);
@@ -62,30 +70,31 @@ const server = http.createServer((req, res) => {
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
       'Content-type': 'text/html',
-    })
+    });
 
     // will be an array, with the five final HTML's,=> then join each array element into a new string
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-    res.end(output)//send back a very simple response
-
+    res.end(output); //send back a very simple response
 
     //product page
   } else if (pathname === '/product') {
     res.writeHead(200, {
       'Content-type': 'text/html',
-    })
-    const product = dataObj[query.id]//retrieve one single data obj
-    const output = replaceTemplate(tempProduct, product)
-    res.end(output) // whole (HTML + CSS).html file
+    });
+    const product = dataObj[query.id]; //retrieve one single data obj
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output); // whole (HTML + CSS).html file
     //api page
   } else if (pathname === '/api') {
     //use script's context dir string
     res.writeHead(200, {
-      'Content-type': 'application/json'
-    })
-    res.end(data) // only receive string
+      'Content-type': 'application/json',
+    });
+    res.end(data); // only receive string
   }
 
   //not found page
@@ -94,11 +103,11 @@ const server = http.createServer((req, res) => {
     //status code is made up by developer
     res.writeHead(404, {
       'Content-type': 'text/html',
-      'made-up-header': 'hahaha'
-    })
-    res.end('<h1>404!</h1>')
+      'made-up-header': 'hahaha',
+    });
+    res.end('<h1>404!</h1>');
   }
-})
+});
 
 //step2: listen to incoming request from client on localhost IP's specific port
 //port: a subAddress of host
@@ -106,4 +115,4 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
   //call run when start listening
   console.log('Listening to request on 8000');
-})
+});
